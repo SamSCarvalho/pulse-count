c66d72abc315c8f06f72df85589fc31118374187
Samuel Santiago <sam.scarvalho17@gmail.com>
2019-10-23 10:01:52 -0300

    analise para o código de contagem de pulsos inverso

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # pulse_count
-Projeto em Arduíno para contagem de pulsos. Podendo assim realizar experimentos para obter uma avaliação da exatidão e precisão dos relógios de placa microcontroladoras.
+Projeto em Arduíno para contagem de pulsos. Podendo assim realizar experimentos para obter uma avaliação da exatidão e precisão dos relógios de placas microcontroladoras.
--- count_pulsos_invert/count_pulsos_invert.ino
+++ count_pulsos_invert/count_pulsos_invert.ino
@@ -1,21 +1,42 @@
-const byte interruptPin = 2; // intr 0
-volatile unsigned long start_sec; // this is to hold start of sec
-int centi_sec;
-void setup()
-{
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(0, update_sec_start, FALLING); // or RISING
+unsigned long int IRQcount;
+int countEvents = 0;
+int pin = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin (9600);
+  attachInterrupt(digitalPinToInterrupt(pin), IRQcounter, FALLING);
+//  TCCR1A = 0;                        // confira timer para operação normal pinos OC1A e OC1B desconectados
+//  TCCR1B = 0;                        // limpa registrador
+//  TCCR1B |= (1<<CS10)|(1 << CS12);   // configura prescaler para 1024: CS12 = 1 e CS10 = 1
+ 
+//  TCNT1 = 0xC2F7;                    // incia timer com valor para que estouro ocorra em 1 segundo
+                                     // 65536-(16MHz/1024/1Hz) = 49911 = 0xC2F7
+//  TIMSK1 |= (1 << TOIE1);   
 }
 
-void loop()
-{
-  // lots of code here
-  // read your time: hr,min,sec;
-  centi_sec = (millis()-start_sec);
-  // printout time..
+void IRQcounter() {
+  if (IRQcount == 32768) {
+    IRQcount = 0;
+    Serial.print("One Second: ");
+  }
+  IRQcount++;
 }
 
-void update_sec_start() // this updates every second.
-{
-  start_sec=millis();
+void loop() {
+  // put your main code here, to run repeatedly:
 }
+
+//ISR(TIMER1_OVF_vect) // interrupção do TIMER1 
+//{
+//  TCNT1 = 0xC2F7; // Renicia TIMER
+//  cli();//disable interrupts
+//  unsigned long int result = IRQcount;
+//  countEvents++;
+//  IRQcount = 0;
+//  sei();//enable interrupts
+//  Serial.print(F("Event: "));
+//  Serial.print(countEvents);
+//  Serial.print(F(" Counted Pulses = "));
+//  Serial.println(result);
+//}
